9b416b8a561560a3204b2e469c9c9dc7d7c7c105
ismail hassan <ismailshariff350@gmail.com>
2025-07-14 15:38:32 -0500

    added my image

--- notes-app/app/index.jsx
+++ notes-app/app/index.jsx
@@ -1,19 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
-const HomeScreen =  () =>  {
+const HomeScreen = () => {
   return (
     <View style={Styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={require('../assets/images/post.png')} style={Styles.image} />
+      <Text style={Styles.title}>Welcome To Notes App</Text>
+      <Text style={Styles.subtitle}>Capture Your thoughts anytime anywhere</Text>
     </View>
   );
-}
+};
 
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20,
+    backgroundColor: '#f8f9fa',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
 
-export default HomeScreen
+export default HomeScreen;
